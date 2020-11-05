a37275e232ae8c0a5e287a2684fda079732e6029
Kim Anh Nguyen <kimanhou@gmail.com>
2020-11-05 19:48:12 +0100

    Contact text goes up

--- src/view/components/Contact/Contact.tsx
+++ src/view/components/Contact/Contact.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Logo from '../Logo/Logo';
 import Navigation from '../Navigation/Navigation';
+import Text from '../LocalisationContext/Text';
 import './Contact.scss';
 
 const Contact : React.FunctionComponent = props => {
@@ -10,12 +11,22 @@ const Contact : React.FunctionComponent = props => {
       <Logo />
       <div className={`contact-content`}>
         <div className={`contact-intro`}>
-          If you have a website project
-          <br></br>
-          feel free to contact me
+          <div className={`contact-intro-line`}>
+            <div className={`contact-intro-line-content`}>
+              <Text english="If you have a website project" french="Si vous avez un projet de site web"/>
+            </div>
+          </div>
+          
+          <div className={`contact-intro-line`}>
+            <div className={`contact-intro-line-content`}>
+              <Text english="feel free to contact me" french="n'hésitez pas à me contacter"/>
+            </div>
+          </div>
         </div>
         <div className={`contact-email`}>
-          <a href="mailto:kimanhou@gmail.com">kimanhou@gmail.com</a>
+            <div className={`contact-email-content`}>
+              <a href="mailto:kimanhou@gmail.com">kimanhou@gmail.com</a>
+            </div>
         </div>
       </div>
     </div>
