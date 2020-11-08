69bb4b6d9b368e8bdfa5d3120df962a621e9f7af
Kim Anh Nguyen <kimanhou@gmail.com>
2020-11-08 16:48:39 +0100

    Updates

---
+++ precache-manifest.7082d352e89be5667e592536d1350d82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f385b7ce8f10f8282452",
+    "url": "./static/css/main.6ec84649.chunk.css"
+  },
+  {
+    "revision": "f385b7ce8f10f8282452",
+    "url": "./static/js/main.f385b7ce.chunk.js"
+  },
+  {
+    "revision": "f93a0cab2fa876dfcee3",
+    "url": "./static/js/1.f93a0cab.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "7b07df5bc1d49de10860853a7b98db8a",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a2c801c38792f87af55455c3a732934d.js"
+  "./precache-manifest.7082d352e89be5667e592536d1350d82.js"
 );
 
 workbox.clientsClaim();
