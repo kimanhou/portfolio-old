81fb6fb421e9768c43470373acc38903c22a8df2
Kim Anh Nguyen <kimanhou@gmail.com>
2020-11-17 11:35:56 +0100

    Cleaning

--- src/view/components/About/Sections/AboutCurrentStatus.tsx
+++ src/view/components/About/Sections/AboutCurrentStatus.tsx
@@ -15,24 +15,24 @@ const AboutCurrentStatus : React.FC<IAboutCurrentStatusProps> = props => {
           &nbsp;&nbsp;&nbsp;&nbsp;<Text english="I left London in Septembre 2019 to " 
                 french="J'ai quitté Londres en septembre 2019 pour "/>
           <AboutKeyword text={<Text english="travel around the world" french="un tour du monde"/>} />
-          <Text english=". I started with Europe before making my way to South East Asia. I was in Thailand when the COVID-19 pandemic hit the world and had to return to France at the end of March 2020." 
-                french=". J'ai commencé par l'Europe avant de continuer mon chemin vers l'Asie du Sud-Est. J'étais en Thaïlande lorsque la pandémie de la COVID-19 a frappé le monde et j'ai ainsi dû rentrer en France fin mars 2020."/>
+          <Text english=". I started in Europe before making my way to South East Asia. I was in Thailand when the COVID-19 pandemic hit the world and had to return to France at the end of March 2020." 
+                french=". J'ai commencé par l'Europe avant de continuer mon chemin vers l'Asie du Sud-Est. J'étais en Thaïlande lorsque la pandémie de la COVID-19 a frappé le monde, ce qui m'a forcée à rentrer en France fin mars 2020."/>
           <br></br>
           &nbsp;&nbsp;&nbsp;&nbsp;<Text english="During lockdown, I was asked by relatives to give a hand on a "
                 french="Pendant le confinement, j'ai été sollicitée par des proches pour donner un coup de main sur un " />
           <a href="http://lescovideastes.fr/" target='_blank'><Text english="Wordpress website" french ="site Wordpress"/></a>
-          <Text english=" and to help two local businesses create their website. To do so, I learned " 
-                french =" et aider deux petits commerces à créer leur site web. Pour cela, j'ai appris "/>
+          <Text english=" and to help two local businesses create their website. This allowed me to expand my skills on both " 
+                french =" et aider deux petits commerces à créer leur site web. Cette expérience m'a permis de développer mes compétences en "/>
           <AboutKeyword text={<Text english="React" french="React"/>} />
-          <Text english=" and Typescript on my own. I also took an online 4 course specialization at Calarts on Coursera to learn more about " 
+          <Text english=" and Typescript. I also took an online 4 course specialization with Calarts on Coursera to learn more about " 
                 french=" et Typescript en autonomie. J'ai également suivi une spécialisation en ligne à Calarts sur Coursera pour en apprendre davantage sur le "/>
           <AboutKeyword text={<Text english="UX and UI design" french="design UX et UI"/>} />
           <Text english="." french="."/>
           <br></br>
           &nbsp;&nbsp;&nbsp;&nbsp;<Text english="I am looking for "
                 french="Je suis à la recherche d'" />
-          <AboutKeyword text={<Text english="short opportunities" french="opportunités courtes"/>} />
-          <Text english=" while waiting for the sanitary situation to improve enough to allow me to keep travelling." french=" en attendant que la situation sanitaire ne s'améliore et de pouvoir reprendre les voyages."/>
+          <AboutKeyword text={<Text english="short-term opportunities" french="opportunités de courte durée"/>} />
+          <Text english=" while waiting for the sanitary situation to improve enough to allow me to resume my travelling." french=" en attendant que la situation sanitaire ne s'améliore et de pouvoir reprendre mon voyage."/>
         </div>
     );
 }
